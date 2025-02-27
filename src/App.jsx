d5ea35abd2335d3fa72ecaffde4a4d5af9d234ad
Sona Angdembe <asonaxaviersm1@gmail.com>
2025-02-27 23:11:04 +0545

    show hide password

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,16 @@
-import React, {useState} from 'react'
+import React , {useState} from 'react'
 
 const App = () => {
- 
-  const [things, setThings] = useState([]) // store the list of an item
-  const [thing, setThing] = useState('') // store the current value
-  
 
+  const [showPassword, setShowPassword] = useState(false)
 
-  const handleAdd =() =>{
-    setThings([...things, thing]) // add new item to an array
-    setThing('') // clear input field
-  }
-
-  const handleDelete = (index) =>{
-    const updatedThings = things.filter((_, id) => id !==index)
-    setThings(updatedThings);
-  }
   return (
     <>
-    <label htmlFor="">Enter the things : </label>
+    <label htmlFor="">Enter Password : </label>
     <input
-     value={thing}    placeholder = 'enter whatever you like'
-    onChange = {(e) => setThing(e.target.value)}
+    type = {showPassword ? 'text' : 'password'}
     />
-    <button onClick ={handleAdd}>Add</button>
-    <br/>
-    <p>Things you entered</p>
-
-    <ul>
-     {
-      things.map((curr, id) =>(
-        <li key={id}>{curr}{''}
-        <button onClick={() => handleDelete(id)}>Delete</button>
-        </li>
-      ))
-     }
-    </ul>
-  
+    <button onClick={() =>setShowPassword(!showPassword) }>{showPassword ? 'Hide' : 'Show' }</button>
     </>
   )
 }
