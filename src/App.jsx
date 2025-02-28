f783ad4a48872adacf52271283b8fbb332bc144f
Sona Angdembe <asonaxaviersm1@gmail.com>
2025-02-28 12:29:24 +0545

    displaying name your enteres

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React,{useState} from 'react'
 
 const App = () => {
-  const [toggle, setToggle] = useState(true)
+  const [DisplayName, setDisplayName] = useState('')
   
   return (
     <>
-    <button onClick={() => setToggle(!toggle)}>{toggle ? 'Off' : 'On'}</button>
+    <h3>Build a React form with an input field where users can type their name. Display the typed name below the input field in real time.</h3>
+    <label htmlFor="">Enter your name : </label>
+    <input type="text"
+    placeholder = 'enter your name'
+    onChange = {(e) => setDisplayName(e.target.value)}
+    />
+    <p>name your entererd : {DisplayName}</p>
     </>
   )
 }
